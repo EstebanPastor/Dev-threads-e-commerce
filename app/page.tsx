535454ac39f6de6124c36c9469d1093e113900a7
Esteban Pastor <estebanandrespastor@gmail.com>
2024-04-03 17:39:37 -0300

    style: :lipstick: Add info section to the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "./components/ui/hero/Hero"
+import Info from "./components/ui/info/Info"
 
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <Info />
     </>
   )
 }
