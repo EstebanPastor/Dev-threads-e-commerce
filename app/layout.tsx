948995a0684a2061f9e458162ef593c7e3ffa69b
Esteban Pastor <estebanandrespastor@gmail.com>
2024-04-17 16:03:25 -0300

    feat: :sparkles: Add cart context and provider for state management

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "./components/ui/navbar/Navbar";
 import AuthContext from "@/context/AuthContext";
 import getCurrentUser from "./(auth)/actions/getCurrentUser";
 import ToasterContext from "@/context/HotToastContext";
+import CartContext from "@/context/CartContext";
 
 const raleway = Raleway({
   subsets: ["latin"],
@@ -28,9 +29,11 @@ export default async function RootLayout({
     <html lang="en">
       <body className={raleway.className}>
         <AuthContext>
-          <ToasterContext />
-          <Navbar user={user!} />
-          {children}
+          <CartContext>
+            <ToasterContext />
+            <Navbar user={user!} />
+            {children}
+          </CartContext>
         </AuthContext>
       </body>
     </html>
